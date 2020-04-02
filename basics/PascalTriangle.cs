56b926c5e333050644e77af7cbeeb4dc02d23c1f
Sinclair Liang <sinclairliang@gmail.com>
2020-04-02 16:14:26 -0700

    need Main()

--- basics/PascalTriangle.cs
+++ basics/PascalTriangle.cs
@@ -1,8 +1,20 @@
+using System;
 public class SolutionCLass {
     public void PascalTriangle(int row) {
         int col = 1;
         for(int i =1; i < row; i++) {
-            
+            for(int space = 1; space <= row-i; space++) {
+                Console.Write(" ");
+            }
+            for(int j = 0; j <= i; j++) {
+                if(j==0||i==0) {
+                    col = 1;
+                } else {
+                    col = col * (i-j+1)/j;
+                }
+                Console.Write("{0}  ", col);
+            }
+            Console.WriteLine();
         }
     }
 }
