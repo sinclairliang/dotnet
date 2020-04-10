2d40b6b70f305b2f0bda70aaab89b5a5d00d19f2
Sinclair Liang <sinclairliang@gmail.com>
2020-04-09 22:07:27 -0700

    added new column in database

--- BookListRazor/BookListRazor/Model/Book.cs
+++ BookListRazor/BookListRazor/Model/Book.cs
@@ -5,8 +5,11 @@ namespace BookListRazor.Model
 {
     public class Book
     {
+        [Key]
         public int Id { get; set; }
+
         public string Name { get; set; }
         public string Author { get; set; }
+        public string ISBN { get; set; }
     }
 }
