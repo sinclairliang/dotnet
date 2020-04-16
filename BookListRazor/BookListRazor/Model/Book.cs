7949acfd576531894476701d37b4a1c2bc0fa40c
Sinclair Liang <sinclairliang@gmail.com>
2020-04-15 18:37:06 -0700

    nneds to change database: just added required items

--- BookListRazor/BookListRazor/Model/Book.cs
+++ BookListRazor/BookListRazor/Model/Book.cs
@@ -8,6 +8,7 @@ namespace BookListRazor.Model
         [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
         public string Author { get; set; }
         public string ISBN { get; set; }
