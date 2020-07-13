6075456be03b760e4c0160eef07383dffae061ff
Sinclair Liang <sinclairliang@gmail.com>
2020-07-12 23:59:39 -0500

    added js

--- BookListMVC/BookListMVC/Controllers/BooksController.cs
+++ BookListMVC/BookListMVC/Controllers/BooksController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +11,27 @@ namespace BookListMVC.Controllers
 {
     public class BooksController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Book Book { get; set };
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
+
+
+        #region API Calls
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToLisyAsync() });
+        }
+        #endregion
     }
 }
