f52c4c2082c25a3c694a5343dd52e1332b9c7653
Sinclair Liang <sinclairliang@gmail.com>
2020-07-21 20:47:41 -0500

    calculator

--- Giraffe/Giraffe/Program.cs
+++ Giraffe/Giraffe/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Giraffe
 {
@@ -6,7 +10,42 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello C#");
+            int mode;
+            Console.WriteLine("Enter Mode, 1 for addition, 2 for subtraction, 3 for Multiplicatation and 4 for division.");
+            mode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Two Numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (mode == 1)
+            {
+                Console.WriteLine(Convert.ToString(num1+num2));
+                return;
+            }
+
+            if(mode == 2)
+            {
+                Console.WriteLine(Convert.ToString(num1 - num2));
+                return;
+            }
+            if(mode == 3)
+            {
+                Console.WriteLine(Convert.ToString(num1 * num2));
+                return;
+
+            }
+            if (mode == 4)
+            {
+                Console.WriteLine(Convert.ToString(num1 / num2));
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Not Supported");
+                return;
+            }
         }
     }
 }
