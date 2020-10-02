45253e4b978afe030be1f4a642c54a98fb36b30d
Sinclair Liang <sinclairliang@gmail.com>
2020-10-02 18:31:55 -0500

    reformatted

--- Reactivity/API/Program.cs
+++ Reactivity/API/Program.cs
@@ -18,11 +18,11 @@ namespace API
         public static void Main(string[] args)
         {
             var host = CreateWebHostBuilder(args).Build();
-            
+
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-                try 
+                try
                 {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
