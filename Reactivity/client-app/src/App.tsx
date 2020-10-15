cf1b482aa6d4a7f05106a95829bb7d0d6d0037fa
Sinclair Liang <sinclair.liang@mutualofomaha.com>
2020-10-14 19:16:35 -0500

    reformatted

--- Reactivity/client-app/src/App.tsx
+++ Reactivity/client-app/src/App.tsx
@@ -1,35 +1,36 @@
-import React, { Component } from 'react';
-import { Header, Icon, List } from 'semantic-ui-react';
+import React, {Component} from 'react';
+import {Header, Icon, List} from 'semantic-ui-react';
 import './App.css';
 import axios from 'axios';
 
 class App extends Component {
-  state = {
-    values: [],
-  };
+    state = {
+        values: [],
+    };
 
-  componentDidMount() {
-    axios.get('http://localhost:5000/api/values').then((response) => {
-      this.setState({
-        values: response.data,
-      });
-    });
-  }
-  render() {
-    return (
-      <div>
-        <Header as="h2">
-          <Icon name="users" />
-          <Header.Content>Reactivity</Header.Content>
-        </Header>
-        <List>
-          {this.state.values.map((value: any) => (
-            <List.Item key={value.id}>{value.name}</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/values').then((response) => {
+            this.setState({
+                values: response.data,
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Header as="h2">
+                    <Icon name="users"/>
+                    <Header.Content>Reactivity</Header.Content>
+                </Header>
+                <List>
+                    {this.state.values.map((value: any) => (
+                        <List.Item key={value.id}>{value.name}</List.Item>
+                    ))}
+                </List>
+            </div>
+        );
+    }
 }
 
 export default App;
