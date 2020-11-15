4022cd92082b0b4ddca95d56e3a17efdbfb27ac0
Sinclair Liang <sinclairliang@gmail.com>
2020-11-14 20:16:38 -0600

    need to work on API ActivitiesController.cs

--- Reactivity/API/Controllers/ActivitiesController.cs
+++ Reactivity/API/Controllers/ActivitiesController.cs
@@ -1,26 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
-using MediatR;
-
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ActivitiesController : ControllerBase
+    public class ActivitiesController
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
-        [HttpGet]
-        public async Task
+        
     }
 }
