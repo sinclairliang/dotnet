a3b107426f8bb83cbd71a9c9400a3c5e77db8917
Sinclair Liang <sinclairliang@gmail.com>
2020-11-14 20:02:13 -0600

    added stash

---
+++ Reactivity/API/Controllers/ActivitiesController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using MediatR;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ActivitiesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task
+    }
+}
